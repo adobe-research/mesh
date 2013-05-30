be6a3cc8610ad288fc655027ff18826a0f0ab4c0
Brent Baker <brbaker@adobe.com>
2013-05-30 10:27:59 -0400

    Additional testing based on codecoverage

--- src/script/test/test_intrinsics.m
+++ src/script/test/test_intrinsics.m
@@ -1718,17 +1718,41 @@ assert_true({ ushiftr(inc(3), 1+3); true });
 // TEST
 
 //////////////////////////////////////////////////
-// foreign interface
+// Array
 //////////////////////////////////////////////////
-myArray = array(4, 0);
-// TODO: public static final ALen _alen = new ALen();
-// TODO: javassist.CannotCompileException
-// alen(myArray);
-assert_equals({ 0 }, { aget(myArray, 0) });
-aset(myArray, 0, 1);
-assert_equals({ 1 }, { aget(myArray, 0) });
 
-// public static final ASet _aset = new ASet();
+// array : { <T> (Int, T) -> Array(T) => <intrinsic> }
+assert_equals({ (true, true, true) }, { a = array(3, true); (aget(a, 0), aget(a, 1), aget(a, 2)) }); // Boolean
+assert_equals({ (2, 2, 2) }, { a = array(3, 2); (aget(a, 0), aget(a, 1), aget(a, 2)) }); // Int
+assert_equals({ (2L, 2L, 2L) }, { a = array(3, 2L); (aget(a, 0), aget(a, 1), aget(a, 2)) }); // Long
+assert_equals({ (2.1, 2.1, 2.1) }, { a = array(3, 2.1); (aget(a, 0), aget(a, 1), aget(a, 2)) }); // Double
+assert_equals({ ((1, 2), (1, 2), (1, 2)) }, { a = array(3, (1,2)); (aget(a, 0), aget(a, 1), aget(a, 2)) }); // Object
+assert_equals({ (2, 2, 2) }, { a = apply(array, (3, 2)); (aget(a, 0), aget(a, 1), aget(a, 2)) }); // apply
+
+
+// aget : { <T> (Array(T), Int) -> T => <intrinsic> }
+assert_equals({ true }, { a = array(1, true); aget(a, 0) }); // Boolean
+assert_equals({ 2 }, { a = array(1, 2); aget(a, 0) }); // Int
+assert_equals({ 2L }, { a = array(1, 2L); aget(a, 0) }); // Long
+assert_equals({ 2.1 }, { a = array(1, 2.1); aget(a, 0) }); // Double
+assert_equals({ (1, 2) }, { a = array(1, (1,2)); aget(a, 0) }); // Object
+assert_equals({ 2 }, { a = array(1, 2); apply(aget, (a, 0)) }); // apply
+
+// aset : { <T> (Array(T), Int, T) -> Array(T) => <intrinsic> }
+assert_equals({ false }, { a = array(1, true); aset(a, 0, false); aget(a, 0) }); // Boolean
+assert_equals({ 1 }, { a = array(1, 2); aset(a, 0, 1); aget(a, 0) }); // Int
+assert_equals({ 1L }, { a = array(1, 2L); aset(a, 0, 1L); aget(a, 0) }); // Long
+assert_equals({ 1.1 }, { a = array(1, 2.1); aset(a, 0, 1.1); aget(a, 0) }); // Double
+assert_equals({ (0, 0) }, { a = array(1, (1,2)); aset(a, 0, (0,0)); aget(a, 0) }); // Object
+assert_equals({ 1 }, { a = array(1, 2); apply(aset, (a, 0, 1)); aget(a, 0) }); // apply
+
+// alen : { <T> Array(T) -> Int => <intrinsic> }
+assert_equals({ 1 }, { a = array(1, true); alen(a) }); // Boolean
+assert_equals({ 1 }, { a = array(1, 2); alen(a) }); // Int
+assert_equals({ 1 }, { a = array(1, 2L); alen(a) }); // Long
+assert_equals({ 1 }, { a = array(1, 2.1); alen(a) }); // Double
+assert_equals({ 1 }, { a = array(1, (1,2)); alen(a) }); // Object
+assert_equals({ 1 }, { a = array(1, 2); apply(alen, (a)) }); // apply
 
 
 
